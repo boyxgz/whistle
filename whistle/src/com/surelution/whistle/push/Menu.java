d991259c98ea0e24d1879fc1596a15566f7f0b42
boyxgz <guangzongsyu@gmail.com>
2013-08-18 00:54:07 +0800

    i

---
+++ whistle/src/com/surelution/whistle/push/Menu.java
@@ -0,0 +1,25 @@
+package com.surelution.whistle.push;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Menu {
+
+    public static void main(String[] args) throws Exception {
+        String cmd = "";
+        InputStreamReader isr = new InputStreamReader(Menu.class.getResourceAsStream("menu.txt"));
+        BufferedReader br = new BufferedReader(isr);
+        String line = br.readLine();
+        while(line != null) {
+            cmd += line;
+            cmd += "\n";
+            line = br.readLine();
+        }
+        br.close();
+        Pusher p = new Pusher();
+        p.setApiUrl("https://api.weixin.qq.com/cgi-bin/menu/create");
+        p.setAccessToken("");
+        p.push(cmd);
+    }
+}
