b693bfc7b35fa9e46c2fdfd92a3f2acf60827d26
boyxgz <boyxgz@msn.com>
2014-07-17 12:10:36 +0800

    i

--- whistle/src/com/surelution/whistle/push/qrcode/Q.java
+++ whistle/src/com/surelution/whistle/push/qrcode/Q.java
@@ -5,14 +5,23 @@ package com.surelution.whistle.push.qrcode;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Hashtable;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
+import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
 import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Reader;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.QRCodeReader;
+import com.google.zxing.common.GlobalHistogramBinarizer;
+import com.google.zxing.multi.GenericMultipleBarcodeReader;
+import com.google.zxing.multi.MultipleBarcodeReader;
 
 /**
  * @author <a href="mailto:guangzong.syu@gmail.com">guangzong</a>
@@ -20,21 +29,40 @@ import com.google.zxing.qrcode.QRCodeReader;
  */
 public class Q {
 
+	private static final Map<DecodeHintType, Object> HINTS;
+	private static final Map<DecodeHintType, Object> HINTS_PURE;
+
+	static {
+		HINTS = new EnumMap<DecodeHintType, Object>(DecodeHintType.class);
+		HINTS.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+		HINTS.put(DecodeHintType.POSSIBLE_FORMATS,
+				EnumSet.allOf(BarcodeFormat.class));
+		HINTS_PURE = new EnumMap<DecodeHintType, Object>(HINTS);
+		HINTS_PURE.put(DecodeHintType.PURE_BARCODE, Boolean.TRUE);
+	}
+
 	public static void main(String[] args) throws Exception {
-		for(int i = 1; i < 600; i++) {
-			String path = "/Users/johnny/temp-zj/";
+		System.out.println(System.currentTimeMillis());
+		Hashtable<DecodeHintType, String> hints = new Hashtable<DecodeHintType, String>();  
+		hints.put(DecodeHintType.CHARACTER_SET, "utf-8");
+		for(int i = 1589; i < 1590; i++) {
+			String path = "/Users/johnny/Google Drive/微信/menguanjia/qr-orig/";
 			path += i;
 			path += ".jpg";
 			try{
 				File file = new File(path);
 				if(file.exists()) {
 					BufferedImage image = ImageIO.read(file);
-					LuminanceSource ls = new BufferedImageLuminanceSource(image);
-					HybridBinarizer hb = new HybridBinarizer(ls);
-					BinaryBitmap bb = new BinaryBitmap(hb);
-					QRCodeReader r = new QRCodeReader();
-					String s = r.decode(bb).getText();
-					String sql = "update gas_station set subscribing_url='" + s + "' where id= " + i + ";";
+				    LuminanceSource source = new BufferedImageLuminanceSource(image);
+				    BinaryBitmap bitmap = new BinaryBitmap(new GlobalHistogramBinarizer(source));
+					
+				    Reader reader = new MultiFormatReader();
+					String s = reader.decode(bitmap, HINTS_PURE).getText();
+					String sql = "INSERT INTO `qr_code`(`version`,`content`,`sn`)VALUES (0,'";
+					sql += s;
+					sql += "','";
+					sql += i;
+					sql += "');";
 //					System.out.print(i);
 //					System.out.print(":");
 //					System.out.println(s);
@@ -42,9 +70,10 @@ public class Q {
 					System.out.println(sql);
 				}
 			}catch(Exception e){
+				e.printStackTrace();
 				System.out.println(" !! update gas_station set subscribing_url='' where id= " + i + ";");
 			}
 		}
-		
+		System.out.println(System.currentTimeMillis());
 	}
 }
