166c709c9f4aa88f03d44a248d4b9ae1a1360e15
boyxgz <boyxgz@msn.com>
2014-12-05 13:36:40 +0800

    i

--- whistle/src/com/surelution/whistle/push/Menu.java
+++ whistle/src/com/surelution/whistle/push/Menu.java
@@ -3,10 +3,14 @@ package com.surelution.whistle.push;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import com.surelution.whistle.core.Auth2Util;
+import com.surelution.whistle.core.Auth2Util.AuthScope;
+
 
 public class Menu {
 
     public static void main(String[] args) throws Exception {
+//    	System.out.println(Auth2Util.buildRedirectUrl("http://shhansi.jios.org/hangzhou-crm/wxPortal/showVouchers", null, AuthScope.BASE));
         String cmd = "";
         InputStreamReader isr = new InputStreamReader(Menu.class.getResourceAsStream("menu.txt"));
         BufferedReader br = new BufferedReader(isr);
