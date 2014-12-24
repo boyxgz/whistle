1eb9dc9b6ea4a7c7a87cf943f5b2b8c546a8a941
boyxgz <boyxgz@msn.com>
2014-12-24 15:22:04 +0800

    i

--- whistle/src/com/surelution/whistle/push/UserInfo.java
+++ whistle/src/com/surelution/whistle/push/UserInfo.java
@@ -13,6 +13,9 @@ import org.json.JSONObject;
  *
  */
 public class UserInfo {
+	private UserInfo(){}
+	
+	private JSONObject jsonObject;
 
 	// 用户的标识
 	private String openId;
@@ -35,93 +38,150 @@ public class UserInfo {
 	// 用户头像
 	private String headImgUrl;
 	
+	private String unionid;
+	
+	private String exactFromJson(String key) {
+		String value = null;
+		try {
+			value = jsonObject.getString(key);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+	
+	public String getUnionid() {
+		if(unionid == null) {
+			unionid = exactFromJson("unionid");
+		}
+		return unionid;
+	}
+
 	public Gender getGender() {
-		return Gender.parse(sex);
+		return Gender.parse(getSex());
 	}
 	
 	public Date getLastSubscribedAt() {
-		return new Date(Integer.parseInt(subscribeTime) * 1000);
+		return new Date(Integer.parseInt(getSubscribeTime()) * 1000);
 	}
 
 	public String getOpenId() {
+		if(openId == null) {
+			openId = exactFromJson("openid");
+		}
 		return openId;
 	}
 
-	public void setOpenId(String openId) {
-		this.openId = openId;
-	}
+//	public void setOpenId(String openId) {
+//		this.openId = openId;
+//	}
 
 	public int getSubscribe() {
+		if(subscribe == 0) {
+			try {
+				subscribe = jsonObject.getInt("subscribe");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
 		return subscribe;
 	}
 
-	public void setSubscribe(int subscribe) {
-		this.subscribe = subscribe;
-	}
+//	public void setSubscribe(int subscribe) {
+//		this.subscribe = subscribe;
+//	}
 
 	public String getSubscribeTime() {
+		if(subscribeTime == null) {
+			subscribeTime = exactFromJson("subscribe_time");
+		}
 		return subscribeTime;
 	}
 
-	public void setSubscribeTime(String subscribeTime) {
-		this.subscribeTime = subscribeTime;
-	}
+//	public void setSubscribeTime(String subscribeTime) {
+//		this.subscribeTime = subscribeTime;
+//	}
 
 	public String getNickname() {
+		if(nickname == null) {
+			nickname = exactFromJson("nickname");
+		}
 		return nickname;
 	}
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
+//	public void setNickname(String nickname) {
+//		this.nickname = nickname;
+//	}
 
 	public int getSex() {
+		if(sex == 0) {
+			try {
+				sex = jsonObject.getInt("sex");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
 		return sex;
 	}
 
-	public void setSex(int sex) {
-		this.sex = sex;
-	}
+//	public void setSex(int sex) {
+//		this.sex = sex;
+//	}
 
 	public String getCountry() {
+		if(country == null) {
+			country = exactFromJson("country");
+		}
 		return country;
 	}
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+//	public void setCountry(String country) {
+//		this.country = country;
+//	}
 
 	public String getProvince() {
+		if(province == null) {
+			province = exactFromJson("province");
+		}
 		return province;
 	}
 
-	public void setProvince(String province) {
-		this.province = province;
-	}
+//	public void setProvince(String province) {
+//		this.province = province;
+//	}
 
 	public String getCity() {
+		if(city == null) {
+			city = exactFromJson("city");
+		}
 		return city;
 	}
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+//	public void setCity(String city) {
+//		this.city = city;
+//	}
 
 	public String getLanguage() {
+		if(language == null) {
+			language = exactFromJson("language");
+		}
 		return language;
 	}
 
-	public void setLanguage(String language) {
-		this.language = language;
-	}
+//	public void setLanguage(String language) {
+//		this.language = language;
+//	}
 
 	public String getHeadImgUrl() {
+		if(headImgUrl == null) {
+			headImgUrl = exactFromJson("headimgurl");
+		}
 		return headImgUrl;
 	}
 
-	public void setHeadImgUrl(String headImgUrl) {
-		this.headImgUrl = headImgUrl;
-	}
+//	public void setHeadImgUrl(String headImgUrl) {
+//		this.headImgUrl = headImgUrl;
+//	}
 
 	public static String loadUserInfoAsJson(String openId) {
 		String ret = null;
@@ -166,28 +226,32 @@ public class UserInfo {
 		}
 		UserInfo user = null;
 		if (null != jsonObject) {
+			user = new UserInfo();
+			user.jsonObject = jsonObject;
 			try {
-				user = new UserInfo();
-				// 用户的标识
-				user.setOpenId(jsonObject.getString("openid"));
-				// 关注状态（1是关注，0是未关注），未关注时获取不到其余信息
-				user.setSubscribe(jsonObject.getInt("subscribe"));
-				// 用户关注时间
-				user.setSubscribeTime(jsonObject.getString("subscribe_time"));
-				// 昵称
-				user.setNickname(jsonObject.getString("nickname"));
-				// 用户的性别（1是男性，2是女性，0是未知）
-				user.setSex(jsonObject.getInt("sex"));
-				// 用户所在国家
-				user.setCountry(jsonObject.getString("country"));
-				// 用户所在省份
-				user.setProvince(jsonObject.getString("province"));
-				// 用户所在城市
-				user.setCity(jsonObject.getString("city"));
-				// 用户的语言，简体中文为zh_CN
-				user.setLanguage(jsonObject.getString("language"));
-				// 用户头像
-				user.setHeadImgUrl(jsonObject.getString("headimgurl"));
+//				
+//				// 用户的标识
+//				user.openId =jsonObject.getString("openid");
+//				// 关注状态（1是关注，0是未关注），未关注时获取不到其余信息
+//				user.subscribe = jsonObject.getInt("subscribe");
+//				// 用户关注时间
+//				user.subscribeTime = jsonObject.getString("subscribe_time");
+//				// 昵称
+//				user.nickname = jsonObject.getString("nickname");
+//				// 用户的性别（1是男性，2是女性，0是未知）
+//				user.sex = jsonObject.getInt("sex");
+//				// 用户所在国家
+//				user.country = jsonObject.getString("country");
+//				// 用户所在省份
+//				user.province = jsonObject.getString("province");
+//				// 用户所在城市
+//				user.city = jsonObject.getString("city");
+//				// 用户的语言，简体中文为zh_CN
+//				user.language = jsonObject.getString("language");
+//				// 用户头像
+//				user.headImgUrl = jsonObject.getString("headimgurl");
+//				
+//				user.unionid = jsonObject.getString("unionid");
 			} catch (Exception e) {
 				if (0 == user.getSubscribe()) {
 					//log.error("用户{}已取消关注", weixinUserInfo.getOpenId());
@@ -239,6 +303,21 @@ public class UserInfo {
 	}
 	
 	public static void main(String[] args) {
-		UserInfo ui = loadUserInfo("oklOVjgRVFE8UiqhmYCEBnkv-Nww");
+		UserInfo ui = loadUserInfo("oEtjTs27yKb7NiNaIzJYlb7gTcr0");
+		System.out.println(ui.getNickname());
+		System.out.println(ui.getUnionid());
+		System.out.println(ui.getCity());
+		System.out.println(ui.getCountry());
+		System.out.println(ui.getHeadImgUrl());
+		System.out.println(ui.getLanguage());
+		System.out.println(ui.getNickname());
+		System.out.println(ui.getOpenId());
+		System.out.println(ui.getProvince());
+		System.out.println(ui.getSex());
+		System.out.println(ui.getSubscribe());
+		System.out.println(ui.getSubscribeTime());
+		System.out.println(ui.getUnionid());
+		System.out.println(ui.getGender());
+		System.out.println(ui.getLastSubscribedAt());
 	}
 }
