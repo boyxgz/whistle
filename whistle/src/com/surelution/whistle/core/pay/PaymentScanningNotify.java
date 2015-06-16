ec4dda87fcf9e120d4347ca368d39893a425e678
boyxgz <boyxgz@msn.com>
2015-06-16 23:36:50 +0800

    i

--- whistle/src/com/surelution/whistle/core/pay/PaymentScanningNotify.java
+++ whistle/src/com/surelution/whistle/core/pay/PaymentScanningNotify.java
@@ -7,12 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 import com.surelution.whistle.core.PlainXmlDocHelper;
